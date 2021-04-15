0646425da81ae0ed11cd2a11ff3ae013c2ed86d2
hongssup <hongssup0202@gmail.com>
2021-04-15 23:34:30 +0900

    Programmers 문자열을 정수로 바꾸기

---
+++ 문자열을 정수로 바꾸기.swift
@@ -0,0 +1,13 @@
+//
+//  문자열을 정수로 바꾸기.swift
+//  algorythm
+//
+//  Created by SeoYeon Hong on 2021/04/15.
+//
+
+import Foundation
+
+//런타임 에러
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
