f6f1e4c3b0d7b75521748c8df929bf37d0724583
hongssup <hongssup0202@gmail.com>
2021-05-09 23:59:17 +0900

    Programmers 평균 구하기

---
+++ 평균 구하기.swift
@@ -0,0 +1,16 @@
+//
+//  평균 구하기.swift
+//  algorythm
+//
+//  Created by SeoYeon Hong on 2021/05/09.
+//
+
+import Foundation
+
+func solution(_ arr:[Int]) -> Double {
+    var sum = 0
+    for i in arr {
+        sum += i
+    }
+    return Double(sum)/Double(arr.count)
+}
