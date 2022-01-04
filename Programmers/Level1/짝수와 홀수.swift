b570980af1d344140e514f939141892601a1a406
hongssup <hongssup0202@gmail.com>
2022-01-04 23:26:52 +0900

    Programmers 짝수와 홀수

---
+++ Programmers/Level1/짝수와 홀수.swift
@@ -0,0 +1,10 @@
+//
+//  짝수와 홀수.swift
+//  algorythm
+//
+//  Created by SeoYeon Hong on 2022/01/04.
+//
+
+func solution(_ num:Int) -> String {
+    return (num % 2 == 0) ? "Even" : "Odd"
+}
